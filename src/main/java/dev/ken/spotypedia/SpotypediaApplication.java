b25dafb14c641cdd3003992a8bdcd950f770e13a
K3NA0N0 <kentaro.aberg@gmail.com>
2025-12-17 15:29:45 +0100

    initial commit

---
+++ src/main/java/dev/ken/spotypedia/SpotypediaApplication.java
@@ -0,0 +1,13 @@
+package dev.ken.spotypedia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotypediaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpotypediaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/ken/spotypedia/SpotypediaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ken.spotypedia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotypediaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
